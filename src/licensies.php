9e565d6f7b411aa6cc20c898cb8f28f7a9a4b8ea
Константин Митин <konmitin.work@gmail.com>
2024-12-02 16:30:25 +0300

    Верстка и адаптива licenses

---
+++ src/licensies.php
@@ -0,0 +1,87 @@
+<div class="licensies">
+    <div class="licensies__container container">
+        <div class="licensies__top">
+            <h2 class="licensies___title title title_h2">
+                Лицензии Битрикс24
+            </h2>
+        </div>
+        <ul class="licensies__list">
+            <li class="licensies__item">
+                <div class="licensies__item-top">
+                    <h4 class="licensies__item-title">Облако</h4>
+                    <p class="licensies__item-price">до <span class="text_purple">12 000</span> руб/мес</p>
+                </div>
+                <div class="licensies__item-main">
+                    <div class="licensies__item-section">
+                        <h5 class="licensies__item-section-title">Преимущества</h5>
+                        <ul class="licensies__item-section-list">
+                            <li class="licensies__item-section-item">Низкий порог вхождения</li>
+                            <li class="licensies__item-section-item">Нет необходимости обслуживать инфраструктуру</li>
+                            <li class="licensies__item-section-item">Новый функционал появляется раньше, чем в коробке</li>
+                        </ul>
+                    </div>
+                    <div class="licensies__item-section">
+                        <h5 class="licensies__item-section-title">Ограничения</h5>
+                        <ul class="licensies__item-section-list">
+                            <li class="licensies__item-section-item">Данные находятся вне корпоративного контура</li>
+                            <li class="licensies__item-section-item">Ограничена возможность кастомизации</li>
+                            <li class="licensies__item-section-item">Интеграция ограничена REST API</li>
+                            <li class="licensies__item-section-item">Зависимость от внешней инфраструктуры</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <button type="button" class="licensies__item-btn" data-popup="cta-license">
+                    Выбрать
+                </button>
+            </li>
+            <li class="licensies__item">
+                <div class="licensies__item-top">
+                    <h4 class="licensies__item-title">коробка</h4>
+                    <p class="licensies__item-price">до <span class="text_purple">99 000</span> руб/год</p>
+                </div>
+                <div class="licensies__item-main">
+                    <div class="licensies__item-section">
+                        <h5 class="licensies__item-section-title">Преимущества</h5>
+                        <ul class="licensies__item-section-list">
+                            <li class="licensies__item-section-item">Контроль над данными — вплоть до закрытого контура</li>
+                            <li class="licensies__item-section-item">Возможность кастомизации и интеграций не ограничена</li>
+                            <li class="licensies__item-section-item">Расширенные инструменты администрирования и настройки</li>
+                            <li class="licensies__item-section-item">Есть штатный функционал, недоступный в облаке</li>
+                        </ul>
+                    </div>
+                    <div class="licensies__item-section">
+                        <h5 class="licensies__item-section-title">Ограничения</h5>
+                        <ul class="licensies__item-section-list">
+                            <li class="licensies__item-section-item">Нужно содержать и обслуживать инфраструктуру</li>
+                            <li class="licensies__item-section-item">Новый функционал появляется позже, чем в облаке</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <button type="button" class="licensies__item-btn" data-popup="cta-license">
+                    Выбрать
+                </button>
+            </li>
+            <li class="licensies__item">
+                <div class="licensies__item-top">
+                    <h4 class="licensies__item-title">коробка энтерпрайз</h4>
+                    <p class="licensies__item-price">до <span class="text_purple">999 000</span> руб/год</p>
+                </div>
+                <div class="licensies__item-main">
+                    <div class="licensies__item-section">
+                        <ul class="licensies__item-section-list">
+                            <li class="licensies__item-section-item">Все преимущества коробки</li>
+                            <li class="licensies__item-section-item">Размещение в кластерных системах высокой доступности</li>
+                            <li class="licensies__item-section-item">Повышенная совместимость с размещением в закрытом контуре</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <button type="button" class="licensies__item-btn" data-popup="cta-license">
+                    Выбрать
+                </button>
+            </li>
+        </ul>
+    </div>
+</div>
